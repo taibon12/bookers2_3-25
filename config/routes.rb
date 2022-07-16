0e1bd7e028fd4ee19ae10de9dc7d7712cf299da4
EC2 Default User <ec2-user@ip-172-31-11-1.ec2.internal>
2022-07-16 12:37:00 +0000

    [3-25]最提出

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "home/about" => "homes#about"
   resources :users, only: [:new, :create, :index, :show, :edit, :update]
+  #espectは[]の中を除外するもの。使う機能が多い場合はこちらを使う方が
   resources :books, except: [:new]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
